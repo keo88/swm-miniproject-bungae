4dc54cb7b7dafe0b488c50c612c30475f771cc64
keo88 <keo.kim0808@gmail.com>
2023-04-24 19:38:35 +0900

    refactor: separate appbar.

--- lib/modules/home/controllers/home_controller.dart
+++ lib/modules/home/controllers/home_controller.dart
@@ -19,4 +19,7 @@ class HomeController extends GetxController {
   void onSearchPressed() {
 
   }
+
+  void onProfilePressed() {
+  }
 }
--- lib/modules/home/views/home_view.dart
+++ lib/modules/home/views/home_view.dart
@@ -2,6 +2,7 @@ import 'package:flutter/material.dart';
 import 'package:swm_miniproject_flutter/common/values/app_colors.dart';
 import 'package:swm_miniproject_flutter/modules/home/controllers/home_controller.dart';
 import 'package:get/get.dart';
+import 'package:swm_miniproject_flutter/modules/widgets/custom_app_bar.dart';
 
 class HomeView extends GetView<HomeController> {
   const HomeView({Key? key}) : super(key: key);
@@ -9,19 +10,9 @@ class HomeView extends GetView<HomeController> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(
-        backgroundColor: AppColors.kPrimaryColor,
-        leading: IconButton(
-          onPressed: controller.onSearchPressed,
-          icon: const Icon(Icons.search)
-        ),
-        actions: const [
-          CircleAvatar(
-            radius: 14,
-            backgroundColor: Color(0xFF383838),
-            child: Icon(Icons.person)
-          ),
-        ],
+      appBar: CustomAppBar(
+        onSearchPressed: controller.onSearchPressed,
+        onProfilePressed: controller.onProfilePressed,
       ),
       body: SizedBox.expand(
         child: ListView.builder(
